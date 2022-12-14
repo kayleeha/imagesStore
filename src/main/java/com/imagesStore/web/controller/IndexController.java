ea6dc9217fb1829cd15c5c9fa70ea0e9eb5af1bd
kayleeha <ampmgrow.go@gmail.com>
2022-12-14 21:19:31 +0900

    my project

---
+++ src/main/java/com/imagesStore/web/controller/IndexController.java
@@ -0,0 +1,19 @@
+package com.imagesStore.web.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class IndexController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		ModelAndView mv = new ModelAndView("index");
+		mv.addObject("data", "Hello");
+		
+		return mv;
+	}
+
+}
