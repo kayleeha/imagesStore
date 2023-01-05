e7b11f14700eeaaa00eb1912b6374c0490f7d075
kayleeha <ampmgrow.go@gmail.com>
2023-01-05 21:42:12 +0900

    rerere

--- src/main/java/com/imagesStore/web/member/vo/MemberVO.java
+++ src/main/java/com/imagesStore/web/member/vo/MemberVO.java
@@ -9,58 +9,76 @@ public class MemberVO {
 	private String id;
 	private String pwd;
 	private String name;
-	private String email;
-	private Date joinDate;
-
+	private String gender;
+	private String age;
+	private String birthday;
+	private String phone;
+	private Date regDate;
+	
+	
 	public MemberVO() {
-		
+		// TODO Auto-generated constructor stub
 	}
-
-	public MemberVO(String id, String pwd, String name, String email) {
+	
+	public MemberVO(String id, String pwd, String name, String gender, String age, String birthday, String phone,
+			Date regDate) {
+		super();
 		this.id = id;
 		this.pwd = pwd;
 		this.name = name;
-		this.email = email;
+		this.gender = gender;
+		this.age = age;
+		this.birthday = birthday;
+		this.phone = phone;
+		this.regDate = regDate;
 	}
-
 	public String getId() {
 		return id;
 	}
-
 	public void setId(String id) {
 		this.id = id;
 	}
-
 	public String getPwd() {
 		return pwd;
 	}
-
 	public void setPwd(String pwd) {
 		this.pwd = pwd;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
-	public String getEmail() {
-		return email;
+	public String getGender() {
+		return gender;
 	}
-
-	public void setEmail(String email) {
-		this.email = email;
+	public void setGender(String gender) {
+		this.gender = gender;
 	}
-
-	public Date getJoinDate() {
-		return joinDate;
+	public String getAge() {
+		return age;
 	}
-
-	public void setJoinDate(Date joinDate) {
-		this.joinDate = joinDate;
+	public void setAge(String age) {
+		this.age = age;
 	}
-
+	public String getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public Date getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
 }
+	
